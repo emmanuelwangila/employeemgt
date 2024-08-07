a0b6ccf657bc7c29bdc3f981a96f3990f8a6e4ec
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-07 10:17:28 +0300

    privarte http for api endpoint

--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Employee } from './employee';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
-export class EmployeeService {}
+export class EmployeeService {
+  private apiServerUrl = '';
+
+  constructor(private http: HttpClient) {}
+}
