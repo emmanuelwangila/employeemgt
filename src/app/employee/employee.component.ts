86c485f9e067a0d749dacd3958b56126017d6e72
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-08 23:34:05 +0300

    importing modules and routes

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Employee } from '../employee';
 import { EmployeeService } from '../employee.service';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css',
 })
