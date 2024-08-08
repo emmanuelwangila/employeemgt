7402d6404f644726d40cf2c97779cbcf3bd1449f
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-08 21:00:34 +0300

    adding styling to the template..

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { AddemployeeComponent } from './addemployee/addemployee.component';
-import { AppComponent } from './app.component';
 
 export const routes: Routes = [
   { path: 'add-employee', component: AddemployeeComponent },
