e3da8824f9740d9478ccb3dbb31a02c9b30eb3e5
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-08 23:40:27 +0300

    configuration routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { EmployeeComponent } from './employee/employee.component';
 
 export const routes: Routes = [
+  { path: 'employees', component: EmployeeComponent },
   { path: 'add-employee', component: AddemployeeComponent },
 ];
