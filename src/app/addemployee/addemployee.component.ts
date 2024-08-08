f6bdf6a8b73bfc602d5e9e32d2e75a91983cedb2
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-08 17:59:03 +0300

    adding an employee using the service  method.. mastery..

--- src/app/addemployee/addemployee.component.ts
+++ src/app/addemployee/addemployee.component.ts
@@ -13,4 +13,37 @@ import { error } from 'console';
 })
 export class AddemployeeComponent {
   title = 'add-employee ';
+
+  public employee: Employee = {
+    id: 0,
+    name: '',
+    email: '',
+    title: '',
+    phone: '',
+    employeeCode: '',
+    imageUrl: '',
+  };
+  constructor(private employeeService: EmployeeService) {}
+
+  public addEmployee(): void {
+    this.employeeService
+      .addEmployees(this.employee)
+      .subscribe((response: Employee) => {
+        alert('Employee added succesfully');
+        console.log('employee added', response);
+
+        (this.employee = {
+          id: 0,
+          name: '',
+          email: '',
+          title: '',
+          phone: '',
+          employeeCode: '',
+          imageUrl: '',
+        }),
+          (error: String) => {
+            console.error('Error adding employee', error);
+          };
+      });
+  }
 }
