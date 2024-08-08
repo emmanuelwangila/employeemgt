b958925f591723397de4ecb591ec13cddc5f558a
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-08 19:11:27 +0300

    code clean ups.. fixes..

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { AddemployeeComponent } from './addemployee/addemployee.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
   { path: 'add-employee', component: AddemployeeComponent },
