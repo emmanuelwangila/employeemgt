b4b59724d71399f2922995f9efd3727b8a41c1de
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-08 16:27:10 +0300

    adding a constructor and function to add an employee..

--- src/app/addemployee/addemployee.component.ts
+++ src/app/addemployee/addemployee.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-addemployee',
   standalone: true,
   imports: [],
   templateUrl: './addemployee.component.html',
-  styleUrl: './addemployee.component.css'
+  styleUrl: './addemployee.component.css',
 })
 export class AddemployeeComponent {
+  title = 'add-employee ';
 
+  public employee: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService) {}
+
+  public addEmployee(employee: Employee) {}
 }
