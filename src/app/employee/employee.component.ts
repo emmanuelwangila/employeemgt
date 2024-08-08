dbcec1a2234fa9450a340ba8b049aceb2b8aac96
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-08 23:28:14 +0300

    function to get employees

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
 
 @Component({
   selector: 'app-employee',
   standalone: true,
   imports: [],
   templateUrl: './employee.component.html',
-  styleUrl: './employee.component.css'
+  styleUrl: './employee.component.css',
 })
 export class EmployeeComponent {
+  public employees: Employee[] = [];
 
+  constructor(private employeeService: EmployeeService) {}
+
+  public getEmployee(): void {
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+        console.log(this.employees);
+      },
+      (error: any) => {
+        console.log('error occured', error);
+      }
+    );
+  }
 }
