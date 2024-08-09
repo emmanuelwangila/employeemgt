3dd129979a4670e2a3a5b4820037ac496a6ec5de
emmanuelwangila <emmanuelwangila1@gmail.com>
2024-08-09 13:12:27 +0300

    fn delete an employee and update list

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -3,6 +3,7 @@ import { Employee } from '../employee';
 import { EmployeeService } from '../employee.service';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { error } from 'console';
 
 @Component({
   selector: 'app-employee',
@@ -33,15 +34,19 @@ export class EmployeeComponent implements OnInit {
   }
 
   public deleteEmployee(employeeId: number): void {
-    this.employeeService
-      .deleteEmployee(employeeId)
-      .subscribe((response: void) => {
+    this.employeeService.deleteEmployee(employeeId).subscribe(
+      (response: void) => {
         console.log('Employee deleted succesfully', response);
         alert('Employee deleted succesfully');
 
         this.employees = this.employees.filter(
           (employee) => employee.id === employeeId
         );
-      });
+      },
+
+      (error: any) => {
+        console.error('Error deleting employee', error);
+      }
+    );
   }
 }
